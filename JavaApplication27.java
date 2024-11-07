bf4314d3a705b50baca4a8963dd502984edf6223
gerasantillan17 <gerapumas122@gmail.com>
2024-11-06 23:50:52 -0600

    Add files via upload

---
+++ JavaApplication27.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package javaapplication27;
+
+/**
+ *
+ * @author gerar
+ */
+public class JavaApplication27 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+      //FOR(CUANTAS VECES SE VA REPETIR LA INSTRUCCION)
+      //WHILE (CUANDO SE REPITE 0 O N VECES)
+      //DO WHILE(AL MENOS UNA VEZ HASTA N VECES)
+      
+      for(int i=1; i<=10; i++ ){
+          System.out.println(i +"-");
+          
+      }
+    }
+    
+}
